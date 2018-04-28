eb1eb00718c980d845a086a04e8af4a75aad6675
Varna Vincent <varnamvmv@gmail.com>
2018-04-27 20:45:21 -0700

    Initial Project Setup

---
+++ database/migrations/2018_04_24_033158_create_order_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('order_id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->double('price', 8, 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['product_id', 'order_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
