8c9c2a84fd84eed94d7ef1d012fe3a24f7cf4f88
Varna Vincent <varnamvmv@gmail.com>
2018-04-30 09:37:57 -0700

    Minor design changes

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom box-shadow">
-  <h5 class="my-0 mr-md-auto font-weight-normal">Burger Project</h5>
+  <h5 class="my-0 mr-md-auto font-weight-normal logo"><a href="/">Burger Project</a></h5>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="/shop">Shop</a>
     <a class="p-2 text-dark" href="#">Enterprise</a>
