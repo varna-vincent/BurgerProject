9cdb571f6fb6ae3321836b068f807186230669a1
Varna Vincent <varnamvmv@gmail.com>
2018-04-29 23:29:09 -0700

    Partial commit - Adding theme

---
+++ resources/views/layouts/nav_light.blade.php
@@ -0,0 +1,11 @@
+
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 box-shadow">
+  <h5 class="my-0 mr-md-auto font-weight-normal text-light">Burger Project</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-light" href="/shop">Shop</a>
+    <a class="p-2 text-light" href="#">Enterprise</a>
+    <a class="p-2 text-light" href="#">Support</a>
+    <a class="p-2 text-light" href="#">Pricing</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Sign up</a>
+</div>
