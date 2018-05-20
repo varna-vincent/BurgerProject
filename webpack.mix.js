7614e5039cdc8c7fc9c5b32ee31df3c9285ec0ae
varna-vincent <vincent.varna@gmail.com>
2018-05-20 14:29:28 -0700

    Minor UI and config changes in Product

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
 	.js('resources/assets/js/loginValidations.js', 'public/js')
+	.js('resources/assets/js/productValidations.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/cover.scss', 'public/css');
