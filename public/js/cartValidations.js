8ef2acbfc15f60137d8bacf84582cf7ad01a3cd7
JaiKarvir <jkarvir@scu.edu>
2018-05-22 14:22:11 -0700

    Added a maximum limit for cart quantity

--- public/js/cartValidations.js
+++ public/js/cartValidations.js
@@ -88,7 +88,7 @@ window.computeTotal = function (index) {
 
 	total_price_value.trim();
 
-	var quantity_pattern = /^[0-9]+$/;
+	var quantity_pattern = /^[0-9]{1,2}$/;
 
 	if (quantity == "") {
 
@@ -96,27 +96,27 @@ window.computeTotal = function (index) {
 		document.getElementById('errdiv_quantity').innerHTML = 'Quantity cannot be blank';
 	} else if (!quantity.match(quantity_pattern)) {
 		$('#errdiv_quantity').addClass('alert alert-danger');
-		document.getElementById('errdiv_quantity').innerHTML = 'Quantity should be a number';
+		document.getElementById('errdiv_quantity').innerHTML = 'Quantity should be maximum 99';
 	} else {
 		$('#errdiv_quantity').removeClass('alert alert-danger');
 		document.getElementById('errdiv_quantity').innerHTML = "";
+
+		var price = price_value.substr(1);
+
+		var each_product_total = parseFloat(quantity) * parseFloat(price);
+		document.getElementById('each_product_total_price_' + index).innerHTML = "$" + each_product_total.toFixed(2);
+
+		var table_length = document.getElementsByTagName("tr");
+		var final_total = parseFloat(0);
+		for (var i = 0; i < table_length.length - 2; i++) {
+			var each_row_total = document.getElementById('each_product_total_price_' + i).innerHTML;
+			var each_row_total_final = each_row_total.substr(1);
+			final_total = final_total + parseFloat(each_row_total_final);
+		}
+
+		var total_price = total_price_value.substr(1);
+		document.getElementById('total_price').innerHTML = "$" + final_total.toFixed(2);
 	}
-
-	var price = price_value.substr(1);
-
-	var each_product_total = parseFloat(quantity) * parseFloat(price);
-	document.getElementById('each_product_total_price_' + index).innerHTML = "$" + each_product_total.toFixed(2);
-
-	var table_length = document.getElementsByTagName("tr");
-	var final_total = parseFloat(0);
-	for (var i = 0; i < table_length.length - 2; i++) {
-		var each_row_total = document.getElementById('each_product_total_price_' + i).innerHTML;
-		var each_row_total_final = each_row_total.substr(1);
-		final_total = final_total + parseFloat(each_row_total_final);
-	}
-
-	var total_price = total_price_value.substr(1);
-	document.getElementById('total_price').innerHTML = "$" + final_total.toFixed(2);
 };
 
 window.deleteProduct = function (orderproduct, index) {
